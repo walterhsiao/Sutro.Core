ec7c212ea581fdebde4dfac6a835113cb91a67da
Greg Meess <greg@sutromachine.com>
2019-12-04 20:54:53 -0800

    Move some classes to gsSlicer to avoid derived code depending on gsEngines

---
+++ engine/SettingsManagerFFF.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using gs.info;
+using gs.interfaces;
+
+namespace gs.engines
+{
+    public class SettingsManagerFFF : SettingsManager<SingleMaterialFFFSettings>
+    {
+        public override List<SingleMaterialFFFSettings> FactorySettings { get
+            {
+                var factory_profiles = new List<SingleMaterialFFFSettings>();
+
+                factory_profiles.Add(new RepRapSettings(RepRap.Models.Unknown));
+                factory_profiles.AddRange(FlashforgeSettings.EnumerateDefaults());
+                factory_profiles.AddRange(PrusaSettings.EnumerateDefaults());
+                factory_profiles.AddRange(MakerbotSettings.EnumerateDefaults());
+                factory_profiles.AddRange(MonopriceSettings.EnumerateDefaults());
+                factory_profiles.AddRange(PrintrbotSettings.EnumerateDefaults());
+
+                return factory_profiles;
+            } }
+
+        public override IUserSettingCollection<SingleMaterialFFFSettings> UserSettings =>
+            new UserSettingsFFF<SingleMaterialFFFSettings>();
+    }
+}
